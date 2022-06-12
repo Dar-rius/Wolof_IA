248f2a6a21b0bc0f89f1135c873efc85daf9b0ab
Dar-rius <mohamedtine17@gmail.com>
2022-06-12 17:18:20 +0000

    update: add css and link for navigation from navbar

--- message_wolof/urls.py
+++ message_wolof/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view),
+    path('', views.index_view, name='home'),
     path('data/', views.data_view, name='data'),
     path('developpeur/', views.developer_view, name='developpeur'),
     path('ia/', views.ai_view, name='ia')
