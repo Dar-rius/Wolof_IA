317e1d8dd4fdab5c926fb72601bd87e0afbbbc97
Dar-rius <mohamedtine17@gmail.com>
2022-06-12 15:43:03 +0000

    change type data for emotions

--- message_wolof/models.py
+++ message_wolof/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
+#choice for sentiments
+SENTIMENTS = (
+    ('Positive', 'Positive'),
+    ('Negative', 'Negative')
+)
+
 # Create your models here.
 class Message_wolof(models.Model):
     message = models.CharField(max_length=300)
     nom_personnalite = models.CharField(max_length=100)
-    emotion = models.CharField(max_length=50)
+    emotion = models.CharField(max_length=50, choices=SENTIMENTS, verbose_name='sentiments')
