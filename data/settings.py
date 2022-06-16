55637e88465f65d6a6b9d690a7cf4d474e193e03
Dar-rius <mohamedtine17@gmail.com>
2022-06-16 21:40:56 +0000

    change debug to fall

--- data/settings.py
+++ data/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
