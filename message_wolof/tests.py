e1429e34ab99f57c7167340e1a2d5dd36d8be68b
Dar-rius <mohamedtine17@gmail.com>
2022-06-22 13:08:33 +0000

    Ajout des testes unitaires

--- message_wolof/tests.py
+++ message_wolof/tests.py
@@ -1,3 +1,9 @@
+from email import message
 from django.test import TestCase
+from .models import Message_wolof
 
 # Create your tests here.
+class Test_data(TestCase):
+    def test_message_wolof(self):
+        Message_wolof.objects.create(message="On effectue les testes", emotion="positive")
+        Message_wolof.objects.create(message="Les testes ont echouer", emotion="Negative")
