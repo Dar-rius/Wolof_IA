b656da36556684a76179aae3573a245276b11a89
Dar-rius <mohamedtine17@gmail.com>
2022-06-22 14:05:11 +0000

    Modification sur les testes

--- message_wolof/tests.py
+++ message_wolof/tests.py
@@ -1,9 +1,14 @@
-from email import message
+#importer les modules
 from django.test import TestCase
 from .models import Message_wolof
 
-# Create your tests here.
+#Creation des cas de testes 
 class Test_data(TestCase):
+    #Une fonction permettant de faire des testes sur les modeles
     def test_message_wolof(self):
-        Message_wolof.objects.create(message="On effectue les testes", emotion="positive")
-        Message_wolof.objects.create(message="Les testes ont echouer", emotion="Negative")
+        messages = Message_wolof()
+        messages.message = "On effectue les testes"
+        messages.emotion = "Positive"
+
+        self.assertEqual( messages.message, "On effectue les testes")
+        self.assertEqual( messages.emotion, "Positive")
