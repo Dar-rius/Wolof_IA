417faec6e272173056799ac528550adebefb3a20
Dar-rius <mohamedtine17@gmail.com>
2022-07-29 15:42:32 +0000

    update

--- message_wolof/views.py
+++ message_wolof/views.py
@@ -11,11 +11,11 @@ from .serializer import Message_wolof_serializers
 import pickle
 
 
-class CustomerView(viewsets.ModelViewSet): 
+"""class CustomerView(viewsets.ModelViewSet): 
     queryset = Message_wolof.objects.all() 
-    serializer_class = Message_wolof_serializers 
+    serializer_class = Message_wolof_serializers """
 
-
+#Une forction permmettant d'utiliser le model afin qu'il fasse des predictions
 def model_predict(data):
     try:
         model = pickle.load(open("./machine_learning/models/languagesTraitor.sav", 'rb'))
@@ -32,6 +32,7 @@ def model_predict(data):
 #La fonction de d'affichage pour la page principale
 def index_view(request):
     # recuperation des donnees des champs du formulaires lorsqu'une methode POST est passe
+    error =""
     form = FormMessage(request.POST)
     #si les donnees sont valides, ils seront enregistre dans la base de donnee
     # et l'utilisateur sera renvoyer vers la page data
@@ -40,21 +41,24 @@ def index_view(request):
         data  = [Message]
         prediction = model_predict(data)
         print(prediction)
+
         if prediction != 0:
             form.save()
+
             #Update data in file csv
             connexion_sql = sqlite3.connect("db.sqlite3")
             df = pd.read_sql_query("SELECT * from message_wolof_message_wolof", connexion_sql)
             df.to_csv("machine_learning/data/messages.csv")
             return redirect('data')
+        else:
+            error = "Veillez ecrire une phrase en wolof"
 
     # Si non rien ne sera stocker dans la base de donnees
     else:
         form = FormMessage
 
-
     #la page dont retourne la fonction avec les champs du formulaires
-    return render(request, 'message_wolof/index.html', {'form': form})
+    return render(request, 'message_wolof/index.html', {'form': form, 'error': error})
 
 
 #une fonction qui lorsque le visiteur arrive sur le route "/" il sera renvoyer vers la page home de l'appli (/message_wolof/home/) 
@@ -68,25 +72,6 @@ def data_view(request):
     #recuperation de toutes les messages en wolof avec leur emotions stocker dans la base de donnees
     messages = Message_wolof.objects.all()
 
-    # Liste de mots en francais qui nous servira de verifier si une phrase est en francais et de le supprimer si c'est le cas. 
-    words = ['bonjour', 'bonsoir', 'bonne soirée', 'bonne soiree', 'soir', 'matin', 'nuit', 'au revoir', 'eau', 'eu ', 'sa va', 'va', 'ca va', 'avais']
-    tableau = {
-        "phrases": ["salut tout le monde", "bonsoir", "toi la "],
-        "languages": ["wolof", "francais", "wolof"]
-    }
-    dataframe = pd.DataFrame(tableau)
-    datafr = dataframe["phrases"][dataframe['languages']=="francais"]
-
-    # Boucle for permettant de supprimer les phrases ecrirte en francais 
-    for word in datafr:
-        data = Message_wolof.objects.filter(message__icontains=word)
-        data.delete()
-
-    #Update data in file csv
-    connexion_sql = sqlite3.connect("db.sqlite3")
-    df = pd.read_sql_query("SELECT * from message_wolof_message_wolof", connexion_sql)
-    df.to_csv("machine_learning/data/messages.csv")
-
     #la page dont retourne la fonction
     return render(request, 'message_wolof/messages.html', {'messages': messages})
 
@@ -96,3 +81,5 @@ def ai_view(request):
 
     #la page dont retourne la fonction
     return render(request, 'message_wolof/services.html', {})
+
+    
