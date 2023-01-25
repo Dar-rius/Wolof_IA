9b868b24afc02538c6b02998741d66d0b4058af9
Dar-rius <mohamedtine17@gmail.com>
2023-01-25 13:08:09 +0000

    update

--- machine_learning/test.py
+++
@@ -1,6 +0,0 @@
-from utils import ScrapSite
-
-scrapping = ScrapSite()
-scrap = scrapping.scanSite("https://www.socolas-blog.com/parler-senegal-vocabulaire-wolof-diola-peul/", "td")
-
-recup = scrapping.recupData(scrap)
--- message_wolof/views.py
+++ message_wolof/views.py
@@ -2,7 +2,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import FormMessage, FormSentences
-from .models import Message_wolof
 import sqlite3
 import pandas as pd
 from rest_framework import viewsets, status
@@ -17,7 +16,7 @@ import sklearn
     serializer_class = Message_wolof_serializers """
 
 #Une forction permmettant d'utiliser le model afin qu'il fasse des predictions sur les messages
-def model_predict(data):
+"""def model_predict(data):
     try:
         model = pickle.load(open("./machine_learning/models/languagesTraitor.sav", 'rb'))
         data = data
@@ -26,7 +25,7 @@ def model_predict(data):
 
     except ValueError as e: 
         return Response(e.args[0], status.HTTP_400_BAD_REQUEST) 
-
+"""
 
 # Creation des fonctions d'affichage pour les differents routes
 
@@ -41,20 +40,7 @@ def index_view(request):
         if form.is_valid():
             Message = form.cleaned_data['message']
             data  = [Message]
-            prediction = model_predict(data)
-            print(prediction)
-
-            if prediction != 0:
-                form.save()
-                #Update data in file csv
-                connexion_sql = sqlite3.connect("db.sqlite3")
-                df = pd.read_sql_query("SELECT * from message_wolof_message_wolof", connexion_sql)
-                df.to_csv("machine_learning/data/messages.csv")
-                return redirect('data')
-            else:
-                error = "Veillez entrer une phrase en wolof"
-
-        # Si non rien ne sera stocker dans la base de donnees
+          # Si non rien ne sera stocker dans la base de donnees
     else:
         form = FormMessage
 
@@ -71,7 +57,7 @@ def redirect_view(request):
 #la fonction d'affichage pour la page "data"
 def data_view(request):
     #recuperation de toutes les messages en wolof avec leur emotions stocker dans la base de donnees
-    messages = Message_wolof.objects.all()
+    messages =  "arret"
 
     #la page dont retourne la fonction
     return render(request, 'message_wolof/messages.html', {'messages': messages})
@@ -97,12 +83,7 @@ def testAI_view(request):
             
             Sentence = form.cleaned_data['sentence']
             data  = [Sentence]
-            value_predict = model_predict(data)
-
-            if value_predict != 0:
-                prediction = "Wolof"
-            else: 
-                prediction ="Français"
+            
     else:
         form = FormSentences
 
